d87bc94e0d51bee2e8b3ef8a55d71c30e7a3d633
Abhishek Sharma <abhishm20@gmail.com>
2023-06-05 15:55:50 +0530

    main file changes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# from api import ask_file
+from api import ask_file
 import os
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'invoice.pdf'), "rb+") as file_object:
-        pass
-        # ask_file(file_object, "What is invoice number?")
+        ask_file(file_object, "What is invoice number?")
